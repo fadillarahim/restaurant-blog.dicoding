00e9f14f57f0239f3e3bb1da26a323f0bd35861b
fadillarahim <fadillarahimximipa1@gmail.com>
2022-09-29 16:31:52 +0700

    last commit before submit

--- src/scripts/DataSource.js
+++
@@ -1,9 +0,0 @@
-import restaurants from '../DATA.json';
-
-
-const getData = ()=>{
-        return console.log(parse(restaurants[1].name));
-    };
-
-
-export default getData;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,27 +1,54 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import '../styles/responsive.css';
 import data from '../DATA.json';
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const listrestElement = document.querySelector('.list-restaurant');
-    const cardElement = document.createElement('div');
-    cardElement.setAttribute('class', 'item');
+    const cardElement = document.querySelector('.card-item');
     const restaurants = data.restaurants;
     restaurants.forEach(resto =>{
         cardElement.innerHTML +=`
-          <div class="card card-item">
-            <img src="${resto.pictureId}" class="card-img-top card-img" alt="${resto.name} Picture">
-            <div class="card-body">
-              <h5 class="card-title">${resto.name}</h5>
-              <h5 class="card-text text-muted">Rating : <span class=ratting>&#9733;</span>${resto.rating}</h5>
-              <p class="card-text card-desc">${resto.description}</p>
-            </div>
-          </div>
+        <article class="post-item">
+        <img class="post-item__thumbnail"
+             src="${resto.pictureId}"
+             alt="${resto.name} restaurant picture" tabindex="0">
+        <div class="post-item__content">
+        <h1 class="post-item__title"><a href="#">${resto.name}</a></h1>  
+        <p class="post-item_city" tabindex="0">
+          located in ${resto.city}<span class=txt-rat> with Ratting </span><span class=ratting>&#9733;</span>${resto.rating}
+          </p>
+          <p class="post-item__description" tabindex="0">${resto.description}</p>
+        </div>
+      </article>
         `
         listrestElement.appendChild(cardElement);
     })
 
+});
+
+const menu = document.querySelector('#menu');
+const jumbotron = document.querySelector('.jumbotron');
+const main = document.querySelector('main');
+const menuBar = document.querySelector('.menu-bar');
+const menuItem = document.querySelectorAll('.nav-item a');
+
+menu.addEventListener('click', function (event) {
+  menuBar.classList.toggle('open');
+  event.stopPropagation();
+});
+
+jumbotron.addEventListener('click', function () {
+  menuBar.classList.remove('open');
+});
+
+main.addEventListener('click', function () {
+  menuBar.classList.remove('open');
+});
+
+menuItem.addEventListener('click', function () {
+  menuBar.classList.remove('open');
 });
